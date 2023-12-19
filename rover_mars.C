1d8d3dd6dac842e7d352193999f96c92436acec3
OsvaldoRoscani <roscaniuni1@gmail.com>
2023-12-20 00:45:01 +0100

    programmazione avanzata

---
+++ rover_mars.C
@@ -0,0 +1,28 @@
+/*
+compito: 
+
+
+
+
+*/
+
+
+
+
+#include<stdio.h>
+
+int main(){
+ int matrice[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+
+
+
+    printf("ciao sotto trovi una matrice");
+
+
+    printf(matrice[1][0]);
+}
